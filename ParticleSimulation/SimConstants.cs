aac90d823d8176d5e90937838aa1cf16c161f40e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-24 17:54:51 +0200

    Changed constants

--- ParticleSimulation/SimConstants.cs
+++ ParticleSimulation/SimConstants.cs
@@ -4,11 +4,11 @@ public class SimConstants
 {
 
     public static double DT = 0.001;
-    public static int N_PARTICLES = 200;
-    public static double BARNES_HUT_DELTA = 0;
+    public static int N_PARTICLES = 100;
+    public static double BARNES_HUT_DELTA = 0.5;
     public static int SIMSIZE = 5000;
     public static int PARTICLES_PER_BOX = 1;
-    public static int STEPS_PER_FRAME = 5;
+    public static int STEPS_PER_FRAME = 10;
     public static double ROTATION_SPEED = 0.005;
     public static int IMG_WIDTH = 1000;
     public static int IMG_HEIGHT = 1000;
